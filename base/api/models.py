801b83ba5e9686bfe12f18ba081a2423f46aa583
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-04-03 22:49:31 +0200

    Feature | model ProgrammingLang | It allows to create categories based on programming language like: Python, JS, Java etc Has fields like: name, short_desc and slug

--- base/api/models.py
+++ base/api/models.py
@@ -13,3 +13,18 @@ class Level(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class ProgrammingLang(models.Model):
+    name = models.CharField(max_length=20, db_index=True)
+    short_desc = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=100, unique=True)
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'Programming language'
+        verbose_name_plural = "Programming languages"
+
+    def __str__(self):
+        return self.name
+    
