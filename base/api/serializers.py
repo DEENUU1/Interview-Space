9a5f3766e4d8d3ceb0381a153bb19843eff1b8ad
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-04-04 13:44:21 +0200

    Feature | ProgrammingLang model serializer | Serializes a ProgrammingLang model instance into JSON-compatible data

--- base/api/serializers.py
+++ base/api/serializers.py
@@ -6,11 +6,22 @@ class LevelModelSerializer(serializers.ModelSerializer):
     """
     Serializes a Level model instance into JSON-compatible data.
     Attributes:
-        Meta: A class that contains metadata about the LevelModelSerializer. It specifies the model class to use and the
-        fields to include in the serialized data.
+        Meta: A class that contains metadata about the LevelModelSerializer.
+        It specifies the model class to use and the fields to include in the serialized data.
     """
     class Meta:
         model = Level
         fields = ('name', 'short_desc')
 
+
+class ProgrammingLangModelSerializer(serializers.ModelSerializer):
+    """
+    Serializes a ProgrammingLang model instance into JSON-compatible data.
+    Attributes:
+        Meta: A class that contains metadata about the ProgrammingLangModelSerializer.
+        It specifies the model class to use and the fields to include in the serialized data.
+    """
+    class Meta:
+        model = ProgrammingLang
+        fields = ('name', 'short_desc')
 
