5ecb4dcf6586f7f1f584dd4b6e90e3793217f60d
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-04-04 13:42:25 +0200

    Feature | Level model serializer | Serializes a Level model instance into JSON-compatible data

---
+++ base/api/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+from .models import Level, ProgrammingLang, Question, Comment, Favourite
+
+
+class LevelModelSerializer(serializers.ModelSerializer):
+    """
+    Serializes a Level model instance into JSON-compatible data.
+    Attributes:
+        Meta: A class that contains metadata about the LevelModelSerializer. It specifies the model class to use and the
+        fields to include in the serialized data.
+    """
+    class Meta:
+        model = Level
+        fields = ('name', 'short_desc')
+
+
