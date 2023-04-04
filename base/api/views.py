0b7dabbb9c6d062f249fbe0436349eed5736603c
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-04-04 15:51:41 +0200

    Feature | ApiKeyAuthentication and pagination | ApiKeyAuthentication class extends the TokenAuthentication class to allow authentiation using API keys passed as query parameters in the request URL APIPagination inherits from PageNumberPagination and it's set with value 10 which means that can display 10 objects on 1 page

--- base/api/views.py
+++ base/api/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import Level, ProgrammingLang, Question, Comment, Favourite
+from .serializers import (
+    LevelModelSerializer,
+    ProgrammingLangModelSerializer,
+    QuestionModelSerializer,
+    CommentModelSerializer,
+    FavouriteModelSerializer
+)
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.exceptions import AuthenticationFailed
+from rest_framework.authtoken.models import Token
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
+from rest_framework.pagination import PageNumberPagination
 
-# Create your views here.
+
+class ApiKeyAuthentication(TokenAuthentication):
+    """
+    This authentication class extends the TokenAuthentication class
+    to allow authentication using API keys passed as query parameters
+    in the request URL.
+    """
+
+    def authenticate(self, request):
+        api_key = request.query_params.get('api_key')
+        if not api_key:
+            return None
+        try:
+            token = Token.objects.get(key=api_key)
+        except Token.DoesNotExist:
+            raise AuthenticationFailed('Invalid API key')
+
+        return (token.user, token)
+
+
+class ApiPagination(PageNumberPagination):
+    """
+    A custom pagination class that extends Django Rest Framework's PageNumberPagination class.
+    This class is designed to handle paginated responses for API views. It sets the default page size to 2 but can be
+    customized by setting the page_size attribute.
+    Attributes:
+    page_size (int): The number of items to include on each page. Defaults to 2.
+    """
+    page_size = 10
